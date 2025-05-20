2fefc2edde91dc28298e75cf69cd3e3c19caee6c
vicenzo <vicenzo@gmail.com>
2025-05-20 14:32:28 -0300

    configurando git

--- scripts/cadastro.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('cadastroForm').addEventListener('submit', function(e) {
-    var senha = document.getElementById('senha').value;
-    var confirmarSenha = document.getElementById('confirmarSenha').value;
-
-    if (senha !== confirmarSenha) {
-        e.preventDefault();
-        alert('As senhas não coincidem!');
-    }
-});
