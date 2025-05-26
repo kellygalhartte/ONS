5dc7e57e150add4e63c0dbc2b4c1b1b9cf3e7ccd
Your Name <you@example.com>
2025-05-26 15:51:51 -0300

    nvnvnv

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+<script>
+    document.getElementById('especialidadesButton').addEventListener('click', function (event) {
+        event.preventDefault(); // Impede o comportamento padrão do link
+        window.location.href = 'especialidades.html'; // Redireciona para especialidades.html
+    });
+</script>
